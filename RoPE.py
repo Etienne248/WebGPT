5320a386c04e6238554b2b1beaeb2c1dfdcc8bb6
Etienne <etienne.pommel@gmail.com>
2023-12-14 11:16:38 +0100

    working RoPE function

--- RoPE.py
+++ RoPE.py
@@ -26,92 +26,7 @@ from torch import nn
 
 
 class RotaryPositionalEmbeddings(nn.Module):
-    """
-    ## RoPE module
-
-    Rotary encoding transforms pairs of features by rotating in the 2D plane.
-    That is, it organizes the $d$ features as $\frac{d}{2}$ pairs.
-    Each pair can be considered a coordinate in a 2D plane, and the encoding will rotate it
-    by an angle depending on the position of the token.
-
-    ### For a pair of features
-
-    Let $x^{(1)}_m$ and $x^{(2)}_m$ be two features of the
-    key or query of any head at position $m$.
-    Or for simplicity assume $x$ has only two features.
-    Then the transformation is,
-
-    \begin{align}
-    RoPE\big(x^{(1)}_m, x^{(2)}_m, m\big) &=
-    \begin{pmatrix}
-    \cos m \theta & - \sin m \theta \\
-    \sin m \theta & \cos m \theta
-    \end{pmatrix}
-    \begin{pmatrix}
-    x^{(1)}_m \\
-    x^{(2)}_m \\
-    \end{pmatrix} \\
-    &=
-    \begin{pmatrix}
-    x^{(1)}_m \cos m\theta - x^{(2)}_m \sin m \theta \\
-    x^{(2)}_m \cos m\theta + x^{(1)}_m \sin m \theta \\
-    \end{pmatrix} \\
-    \end{align}
-
-    where $\theta$ is a constant angle. The other pairs of features are transformed similarly.
-
-    ### Attention is relative
-
-    For a pair of features, dot-product attention score between two positions $m$ and $n$ would be
-
-    \begin{align}
-    \Big \langle RoPE\big(x^{(1)}_m, x^{(2)}_m, m\big),  RoPE\big(x^{(1)}_n, x^{(2)}_n, n\big) \Big \rangle &= \\
-    (x^{(1)}_m \cos m\theta - x^{(2)}_m \sin m \theta)(x^{(1)}_n \cos n\theta - x^{(2)}_n \sin n \theta) &+ \\
-    (x^{(2)}_m \cos m\theta + x^{(1)}_m \sin m \theta)(x^{(2)}_n \cos n\theta + x^{(1)}_n \sin n \theta) &= \\
-    x^{(1)}_m x^{(1)}_n (\cos m\theta \cos n\theta + \sin m \theta \sin n \theta) &+ \\
-    x^{(1)}_m x^{(2)}_n (-\cos m\theta \sin n\theta + \sin m \theta \cos n \theta) &+ \\
-    x^{(2)}_m x^{(1)}_n (-\sin m\theta \cos n\theta + \cos m \theta \sin n \theta) &+ \\
-    x^{(2)}_m x^{(2)}_n (\sin m\theta \sin n\theta + \cos m \theta \cos n \theta) &= \\
-
-    x^{(1)}_m x^{(1)}_n \cos (m - n) \theta +
-    x^{(1)}_m x^{(2)}_n \sin(m - n) \theta &+ \\
-    - x^{(2)}_m x^{(1)}_n \sin (m - n) \theta +
-    x^{(2)}_m x^{(1)}_n \cos (m - n) \theta &= \\
-
-    \big(x^{(1)}_m \cos (m - n)\theta - x^{(2)}_m \sin (m - n) \theta\big) x^{(1)}_n &+ \\
-    \big(x^{(2)}_m \cos (m - n)m\theta + x^{(1)}_m \sin (m - n) \theta\big) x^{(2)}_n  &= \\
-
-    \Big \langle RoPE\big(x^{(1)}_m, x^{(2)}_m, m - n\big),  RoPE\big(x^{(1)}_n, x^{(2)}_n, 0\big) \Big \rangle
-    \end{align}
-
-    This shows that for dot-production attention the rotary encodings gives relative attention.
-
-    ### For all features
-
-    The features are grouped into pairs and handled as above. They use a different $\theta$ for each pair.
-
-    The paper suggests using $\Theta = {\theta_i = 10000^{\frac{2(i-1)}{d}}, i \in [1, 2, ..., \frac{d}{2}]}$
-    for the $\frac{d}{2}$ pairs of features.
-
-    We pair feature $i$ with feature $i + \frac{d}{2}$. So for position $m$ we transform
-
-    \begin{align}
-    \begin{pmatrix}
-    x^{(i)}_m \\
-    x^{(i + \frac{d}{2})}_m
-    \end{pmatrix}
-    \end{align}
-
-    to
-
-    \begin{align}
-    \begin{pmatrix}
-    x^{(i)}_m \cos m \theta_i - x^{(i + \frac{d}{2})}_m \sin m \theta_i \\
-    x^{(i + \frac{d}{2})}_m \cos m\theta_i + x^{(i)}_m \sin m \theta_i \\
-    \end{pmatrix} \\
-    \end{align}
-    """
-
+   
     def __init__(self, d: int, base: int = 10_000):
         """
         * `d` is the number of features $d$
@@ -133,7 +48,7 @@ class RotaryPositionalEmbeddings(nn.Module):
             return
 
         # Get sequence length
-        seq_len = x.shape[-1]
+        seq_len = x.shape[-2]
 
         # $\Theta = {\theta_i = 10000^{-\frac{2(i-1)}{d}}, i \in [1, 2, ..., \frac{d}{2}]}$
         theta = 1. / (self.base ** (torch.arange(0, self.d, 2).float() / self.d)).to(x.device)
@@ -149,15 +64,15 @@ class RotaryPositionalEmbeddings(nn.Module):
         idx_theta2 = torch.cat([idx_theta, idx_theta], dim=1)
 
         # Cache them
-        self.cos_cached = idx_theta2.cos()[None, None, :, :]
-        self.sin_cached = idx_theta2.sin()[None, None, :, :]
+        self.cos_cached = idx_theta2.cos()#[None, None, :, :]
+        self.sin_cached = idx_theta2.sin()#[None, None, :, :]
 
     def _neg_half(self, x: torch.Tensor):
         # $\frac{d}{2}$
         d_2 = self.d // 2
 
         # Calculate $[-x^{(\frac{d}{2} + 1)}, -x^{(\frac{d}{2} + 2)}, ..., -x^{(d)}, x^{(1)}, x^{(2)}, ..., x^{(\frac{d}{2})}]$
-        return torch.cat([-x[:, :, :, d_2:], x[:, :, :, :d_2]], dim=-1)
+        return torch.cat([-x[..., d_2:], x[..., :d_2]], dim=-1)
 
     def forward(self, x: torch.Tensor):
         """
@@ -183,8 +98,7 @@ class RotaryPositionalEmbeddings(nn.Module):
         # \end{align}
         #
         # for $i \in {1, 2, ..., \frac{d}{2}}$
-        print(self.cos_cached[:x.shape[-2]])
-        print(self.sin_cached)
+        print(self.cos_cached)
         x_rope = (x_rope * self.cos_cached[:x.shape[-2]]) + (neg_half_x * self.sin_cached[:x.shape[-2]])
 
         return torch.cat((x_rope, x_pass), dim=-1)
